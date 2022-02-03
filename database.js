c817a2ac2ab35bd0d5b528185860ca0651fe0ea2
JAmoMES <atipat.pa@ku.th>
2022-02-04 03:52:43 +0700

    add http auth request

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require("sequelize")
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './todos_database.sqlite'
+  dialect: "sqlite",
+  storage: "./database/todos_database.sqlite",
 })
 
 sequelize
   .authenticate()
-  .then(() => console.log('Connected to database'))
+  .then(() => console.log("Connected to database"))
   .catch((error) => console.error(error))
 
 module.exports = sequelize
